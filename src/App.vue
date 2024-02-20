31fe79d7570361234c2dda36756c149c7785127c
MaxineVSG <maxine.grohol@akerolabs.com>
2024-02-20 15:21:40 +0000

    remove unecessary code

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,39 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld />
-    </v-main>
+    <v-container>
+      <v-card
+        title="Vuetify Storybook Style Guide"
+        subtitle="Storybook of vuetify components with Akero theme styling and guidelines."
+        class="b b-primary"
+        variant="tonal"
+        color="primary"
+      >
+        <v-container>
+          <strong>How To Run</strong>
+          <v-list class="d-flex flex-column ga-3">
+            <v-list-item class="bg-primary-light d-inline-flex rounded"
+              title="- nvm use 20"
+            ></v-list-item>
+            <v-list-item class="bg-primary-light d-inline-flex rounded"
+              title="- npm install"
+            ></v-list-item>
+            <v-list-item class="bg-primary-light d-inline-flex rounded"
+              title="- npm run storybook"
+            ></v-list-item>
+          </v-list>
+
+          <strong>Live Link</strong>
+          <v-list class="d-flex flex-column ga-3">
+            <v-list-item link class="bg-primary-light d-inline-flex rounded"
+              title="https://65d494725a7a61a7a3ad2c25-fyzqnyozdz.chromatic.com/"
+              href="https://65d494725a7a61a7a3ad2c25-fyzqnyozdz.chromatic.com/"
+              target="_blank"
+            ></v-list-item>
+          </v-list>
+        </v-container>
+      </v-card>
+
+    </v-container>
   </v-app>
 </template>
 
