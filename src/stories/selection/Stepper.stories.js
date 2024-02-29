57e26e6728b960e3bec63a465f04a799a6949f6f
MaxineVSG <maxine.grohol@akerolabs.com>
2024-02-29 15:12:04 +0000

    Selection Components

---
+++ src/stories/selection/Stepper.stories.js
@@ -0,0 +1,18 @@
+import Stepper from './Stepper.vue';
+
+export default {
+  title: 'Selection/Stepper',
+  component: Stepper,
+  tags: ['autodocs'],
+  argTypes: {
+    mobile: {
+      control: {
+        type: 'boolean',
+      }
+    }
+  },
+};
+
+export const Standard = {
+  args: {},
+};
---
+++ src/stories/selection/Stepper.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-stepper :items="['Step 1', 'Step 2', 'Step 3']" color="primary" :mobile="mobile">
+    <template v-slot:item.1>
+      <v-card-blank title="Step One"></v-card-blank>
+    </template>
+
+    <template v-slot:item.2>
+      <v-card-blank title="Step Two"></v-card-blank>
+    </template>
+
+    <template v-slot:item.3>
+      <v-card-blank title="Step Three"></v-card-blank>
+    </template>
+  </v-stepper>
+</template>
+
+<script>
+
+export default {
+  name: 'stepper',
+
+  props: {
+    mobile: {
+      type: Boolean,
+      default: false,
+    }
+  },
+
+  setup() {
+    return {};
+  },
+};
+</script>
